c5722f5d27507404cbb2b0f16be37d312900a931
Timur_etc <48713849+TimurNet@users.noreply.github.com>
2025-03-10 17:17:38 +0300

    Update code.ts

--- code.ts
+++ code.ts
@@ -1,6 +1,6 @@
 /**
  * E-Style Plugin
- * Copyright (C) 2025 [Timur]
+ * Copyright (C) 2025 [Timur Iakshimbetov]
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
